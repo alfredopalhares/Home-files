165eaf2c50dd37d705548643e5193ff400e7546b
Alfredo Palhares <masterkorp@gmail.com>
2011-02-21 22:04:56 +0000

    Changed the uppercase vars to lowercase the wpa_supplicant script

--- scripts/wpa_sup.sh
+++ scripts/wpa_sup.sh
@@ -2,19 +2,19 @@
 # Simple script that connects o a wireless network
 
 #define the interface
-ETH=wlan0
-CFG=/home/masterkorp/scripts/ismai.cfg
+eth=wlan0
+cfg=/home/masterkorp/scripts/ismai.cfg
 
 # terminate current network
 wpa_cli terminate
 
 # Connect the the network
 sleep 5
-wpa_supplicant -B -d -Dwext -i $ETH -c $CFG
+wpa_supplicant -B -d -Dwext -i $eth -c $cfg
 
 sleep 5
 # kill any other DHCP services
-dhcpcd -x $ETH 
+dhcpcd -x $eth 
 
 # Start new dhcp request
-dhcpcd $ETH
+dhcpcd $eth

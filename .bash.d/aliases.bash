29fd86980605dfb6ffb7e784abacac42d9536d3a
Alfredo Palhares <masterkorp@gmail.com>
2011-03-24 09:50:31 +0000

    Added an alias to minecraft using a ssh tunnel

--- .bash.d/aliases.bash
+++ .bash.d/aliases.bash
@@ -14,3 +14,6 @@ alias gls='git ls-files'
 alias glg='git log'
 alias gp='git push'
 alias gpl='git pull'
+
+# Minecraft alias to use the ssh tunnel at port 9999
+alias minecraft_socks='java -Xmx1024M -Xms512M -DsocksProxyHost=127.0.0.1 -DsocksProxyPort=9999 -cp     /usr/share/minecraft/minecraft.jar net.minecraft.LauncherFrame'

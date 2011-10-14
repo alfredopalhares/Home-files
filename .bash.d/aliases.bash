8c8a4752c4670d975b15a6f123408704935ec61b
Alfredo Palhares <masterkorp@gmail.com>
2011-10-14 18:20:58 +0100

    Add alias to shutdown, restart and suspend with console kit

--- .bash.d/aliases.bash
+++ .bash.d/aliases.bash
@@ -15,5 +15,11 @@ alias glg='git log'
 alias gp='git push'
 alias gpl='git pull'
 
+# Alias to shutdown using policykit
+alias ck_shutdown='dbus-send --system --print-reply --dest="org.freedesktop.ConsoleKit" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop'
+alias ck_reboot='dbus-send --system --print-reply --dest="org.freedesktop.ConsoleKit" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Restart'
+alias ck_suspend='dbus-send --system --print-reply --dest="org.freedesktop.UPower" /org/freedesktop/UPower org.freedesktop.UPower.Suspend'
+# alias ck_hibernate='dbus-send --system --print-reply --dest="org.freedesktop.UPower" /org/freedesktop/UPower org.freedesktop.UPower.Hibernate'
+
 # Minecraft alias to use the ssh tunnel at port 9999
 alias minecraft_socks='java -Xmx1024M -Xms512M -DsocksProxyHost=127.0.0.1 -DsocksProxyPort=9999 -cp     /usr/share/minecraft/minecraft.jar net.minecraft.LauncherFrame'

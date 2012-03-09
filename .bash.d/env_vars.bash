6706dcf0e65d97cae4ed7a362b6dba5eed2c5d85
Alfredo Palhares <masterkorp@masterkorp.net>
2012-03-09 11:04:56 +0100

    Remove all files irrelevant to the Boobierack machine

--- .bash.d/env_vars.bash
+++
@@ -1,6 +0,0 @@
-#!bash
-
-# File for setting enviroment vars
-
-# Set the vim as main editor
-export EDITOR="vim"
--- scripts/record_desktop.sh
+++
@@ -1,5 +0,0 @@
-# Easy way of record the desktop
-dimensions=`xdpyinfo | grep 'dimensions:'|awk '{print $2}'`
-output="/tmp/out.mpg"
-
-ffmpeg -f x11grab -s  $dimensions -r 25 -i :0.0 -sameq $output

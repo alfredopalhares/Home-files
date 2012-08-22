1a042b949809aa087e3af755632784f68238f03d
Alfredo Palhares <masterkorp@masterkorp.net>
2012-08-22 17:08:30 +0100

    Bash: Voxmedia environment vars
    Chef stuff
    Campfire

--- .bash.d/env_vars.bash
+++ .bash.d/env_vars.bash
@@ -4,3 +4,11 @@
 
 # Set the vim as main editor
 export EDITOR="vim"
+
+# Set chef vars
+export CHEF_USER="alfredo"
+export OPSCODE_USER="alfredo"
+export ORGNAME="voxmedia"
+
+# For the capistrano deploy
+export CAMPFIRE_ACCOUNT="sbnation"
